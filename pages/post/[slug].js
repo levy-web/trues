0de68c52c3f7fb018bdc002074d93ca8a28b5585
levy <levymuguro@gmail.com>
2022-09-04 23:35:58 +0300

    first commit

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {useRouter} from 'next/router'
 
 import {getPosts, getPostDetails} from '../../services';
 
-import {PostDetail, Categories, PostWidget,Author, Comments, CommentsForm} from '../../components';
+import {PostDetail, Categories, PostWidget,Author, Comments, CommentsForm, Loader} from '../../components';
 
 
 const PostDetails = ({post}) => {
+    const router = useRouter();
+
+    if(router.isFallback){
+        return<Loader/>
+    }
+
+
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
@@ -43,7 +51,7 @@ export async function getStaticPaths(){
 
     return{
         paths:posts.map(({node:{slug}}) => ({params:{slug}})),
-        fallback: false,
+        fallback: true,
 
     }
 }
