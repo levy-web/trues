1591da3303079a5f6246397775d73a6ea4b4311e
levy <levymuguro@gmail.com>
2022-10-25 07:41:08 +0300

    trues

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
                 <div  className="md:float-left block">
                     <Link href="/">
                         <span className="cursor-pointer font-bold text-4xl text-black hover:text-pink-600">
-                            TRUtv
+                            TRues
                         </span>
                     </Link>
 
