c72cd632082ecfcd9ee77bcf2268097a5f06c90b
levy <levymuguro@gmail.com>
2022-10-26 19:02:55 +0300

    order

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -18,7 +18,7 @@ const PostWidget = ({categories, slug}) => {
   }, [slug])
   console.log(relatedPosts)
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 m-4 pb-12 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8 lg: m-4">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
       {relatedPosts.map((post) => (
         <div key={post.title} className="flex items-center w-full mb-4">
