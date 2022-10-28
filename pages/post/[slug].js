7721cacd9028fb347f175592efef03f878589209
levy <levymuguro@gmail.com>
2022-10-28 11:38:31 +0300

    final

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -16,6 +16,14 @@ const PostDetails = ({post}) => {
 
   return (
     <div className='container mx-auto px-10 mb-8'>
+        <head>
+          <>
+          <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4189/e740a3298f9156d68d059bd1048afbc4e8b74fa0/lib.js"></script>
+          <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4208/771547afc51f1af278ab679446c0c3e43c477093/lib.js"></script>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1048957289759461" crossorigin="anonymous"></script>
+          </>
+
+        </head>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
             <div className='col-span-1 lg:col-span-8'>
                 <PostDetail post={post}/>
