0bb911b8dab619d81e8f8b1ea1834897e94016bf
levy <levymuguro@gmail.com>
2022-10-28 09:26:21 +0300

    addscripts

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,11 @@ export default function Home ({posts})  {
       <Head>
         <title>trues</title>
         <link rel="icon" href="/favicon.ico" />
-        <Link rel='stylesheet' href="./adds/addscripts.js"></Link>
+        <>
+        <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4189/e740a3298f9156d68d059bd1048afbc4e8b74fa0/lib.js"></script>
+        <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4208/771547afc51f1af278ab679446c0c3e43c477093/lib.js"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1048957289759461" crossorigin="anonymous"></script>
+        </>
 
       </Head>
       <FeaturedPosts/>
