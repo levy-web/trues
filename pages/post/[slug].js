30def4ba6bc9efdf529402d5de4b98e098c34fbd
levy <levymuguro@gmail.com>
2022-10-28 11:51:10 +0300

    push

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -21,6 +21,8 @@ const PostDetails = ({post}) => {
           <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4189/e740a3298f9156d68d059bd1048afbc4e8b74fa0/lib.js"></script>
           <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4208/771547afc51f1af278ab679446c0c3e43c477093/lib.js"></script>
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1048957289759461" crossorigin="anonymous"></script>
+          <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4216/060e5dac4ffe7de0df2f8a7aa9799a7728841c05/lib.js"></script>
+          <script type="text/javascript"> new PushSubscriber(window, {}).subscribe().catch(function (result) {});</script>
           </>
 
         </head>
