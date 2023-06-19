be32d4e23e88eec099e9cb264a329e27afdee97f
levy <lnjiru43@gmail.com>
2023-06-19 08:00:03 +0300

    space

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home ({posts})  {
         <link rel="icon" href="/bg.jpg" />
         <>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1048957289759461" crossorigin="anonymous"></script>
+        
         <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4189/e740a3298f9156d68d059bd1048afbc4e8b74fa0/lib.js"></script>
         <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4208/771547afc51f1af278ab679446c0c3e43c477093/lib.js"></script>
         <script type="text/javascript" src="https://cdn.diclotrans.com/sdk/v1/4218/382a01859f1d4c058ef76af6ea85dc01c494619f/lib.js"></script>
